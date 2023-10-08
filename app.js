75f29141e6da3b5c053f8588e6c2f9d4f5908698
Shivam pankaj yadav <115870819+SHIVAM-YADU@users.noreply.github.com>
2023-10-08 18:57:14 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
+const formContainer = document.querySelector('.form-container');
+const loginForm = document.querySelector('#sign-in-form');
+const registerFrom = document.querySelector('#sign-up-form');
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
-});
-
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
-	} else {
-		header.style.backgroundColor = 'transparent';
-	}
-});
-
-menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
-	});
-});
+const switchForm = (form) => {
+    if(form == 'register'){
+        if(window.innerWidth > 800){
+            formContainer.style.left = `50%`;
+        }
+        loginForm.style.marginLeft = `-150%`;
+        registerFrom.style.marginLeft = `-100%`;
+    } else{
+        if(window.innerWidth > 800){
+            formContainer.style.left = `0%`;
+        }
+        loginForm.style.marginLeft = `0%`;
+        registerFrom.style.marginLeft = `50%`;
+    }
+}
